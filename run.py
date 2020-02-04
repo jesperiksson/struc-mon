3c6ed277d40918172ac4836ee894e8523ed51810
jesperiksson <55392720+jesperiksson@users.noreply.github.com>
2020-02-04 13:43:13 +0100

    Create run.py
    Adding the run file by copying its contents into a file using the website

---
+++ run.py
@@ -0,0 +1,10 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import matplotli.pyplot as plt
+
+data = keras.#datset
+
+(train_samples, train_labels), (test_samples, test_labels) = data.load_data()
+
+class_names = ['functioning', 'broken'] 
