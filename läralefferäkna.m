093bcfd865a5b08871ba237102f17313ae34e1ba
pontusml <60692711+pontusml@users.noreply.github.com>
2020-02-27 09:51:03 +0100

    Update and rename läralefferäkna to läralefferäkna.m

--- läralefferäkna.m
+++ läralefferäkna.m
@@ -8,11 +8,12 @@ for i=1:200
         intervall(i,n)= intervall(i,n-1)+int;
     end
 end
-header = {'timestep1','timestep2','timestep3', 'timestep4'}
-intervall=[header; num2cell(intervall)]
+header = {'timestep1','timestep2','timestep3', 'timestep4'};
+intervall=[header; num2cell(intervall)];
+
 
 % Convert cell to a table and use first row as variable names
-T = cell2table(intervall(2:end,:),'VariableNames',intervall(1,:))
+T = cell2table(intervall(2:end,:),'VariableNames',intervall(1,:));
  
 % Write the table to a CSV file
-writetable(T,'myDataFile.csv')
+writetable(T,'intervall.csv');
