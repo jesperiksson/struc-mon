d8b13e33c132d845041a5982f42edbd75fabff00
Jesper Eriksson <55392720+jesperiksson@users.noreply.github.com>
2021-04-08 17:01:14 +0200

    Delete train_scheme.sh

--- train_scheme.sh
+++
@@ -1,9 +0,0 @@
-python main.py --mode train --date 2020-10-27 2020-10-28
-
-python main.py --load  --mode train --date 2020-10-28 2020-10-29
-python main.py --load  --mode train --date 2020-11-18 2020-11-19
-python main.py --load  --mode train --date 2020-12-08 2020-12-09
-python main.py --load  --mode train --date 2020-12-28 2020-12-29
-python main.py --load  --mode train --date 2021-01-07 2021-01-08
-python main.py --load  --mode train --date 2021-02-16 2021-02-17
-python main.py --load  --mode train --date 2021-03-10 2021-03-10
